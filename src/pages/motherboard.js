debf2ac62c2358707989cde81c3586eed9e5b927
shahidmonowarr <shahidmonowar21@gmail.com>
2023-07-30 18:02:01 +0600

    categories product Motherboard fetched

---
+++ src/pages/motherboard.js
@@ -0,0 +1,33 @@
+import RootLayout from "@/components/Layouts/RootLayout";
+import Product from "@/components/UI/Product";
+
+const Motherboard = ({ products }) => {
+  return (
+    <div className="container mx-auto">
+      <div className="flex flex-wrap mx-3">
+        {products?.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Motherboard;
+
+Motherboard.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "http://localhost:5000/products?category=Motherboard"
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      products: data?.data,
+    },
+  };
+};
